fd1314fbe58e6edc2199b23503d98aff5d69fb3a
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-06-08 20:47:51 -0400

    map is up and working

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var map;
-
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.7413549, lng: -73.9980244, zoom: 13
-  }});
+    center: {lat: 40.741, lng: -73.9980244},
+    zoom: 13
+  });
 }
